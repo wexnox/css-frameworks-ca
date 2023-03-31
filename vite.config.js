5b0385c09618daa4eafa910b073e361cf5c34640
Lasse <lasseronnlie@hotmail.com>
2023-03-31 05:28:02 +0200

    added bootstrap content

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 // import {resolve} from 'path'
-import path from 'path'
+import path, {resolve} from 'path'
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-    // root: path.resolve(__dirname, 'src'),
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                feed: resolve(__dirname, 'pages/feed/index.html'),
+                profile: resolve(__dirname, 'pages/profile/index.html'),
+            },
+
+        },
+    },
     server: {
         hot: true
     },
