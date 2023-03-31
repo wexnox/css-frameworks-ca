831311690566928fab0d59bd95b719ab67631f3c
Lasse <lasseronnlie@hotmail.com>
2023-03-31 11:31:36 +0200

    config bootstrap variables

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,8 @@
-import '/src/assets/scss/index.scss'
-
 // Import our custom CSS
 import '/src/assets/scss/styles.scss'
+
+// Import all of Bootstrap's CSS
+import '/src/assets/scss/index.scss'
 
 // Import all of Bootstrap's JS
 import 'bootstrap'
